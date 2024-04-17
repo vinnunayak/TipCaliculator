6d5c02fc85cc48e7eff2392734e58d8d4f10a706
vbanavath <vbanavath@innominds.com>
2024-04-17 12:45:47 +0530

    Initial Commit

---
+++ TipCaliculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCaliculator
+//
+//  Created by Vinod Nayak Banavath on 17/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
